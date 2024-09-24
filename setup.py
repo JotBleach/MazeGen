01489c42595e8684d410c2ff9bd6ad66f845d979
JotBleach <aendal522@gmail.com>
2024-09-24 00:17:40 -0500

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='maze-generator-jotbleach',
-    version='1.0.0',
+    version='1.0.1',
     description='A Python library for generating and visualizing mazes with custom properties.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/maze-generator',
+    author='JotBleach',
+    author_email='jotbleach@gmail.com',
+    url='https://github.com/JotBleach/maze_generator',
     license='MIT',
     packages=find_packages(),
     install_requires=[
